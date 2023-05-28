598a8257ca4005226eb6d7b6f1fdbdca8262aa92
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-28 16:19:16 +0600

    fully functional

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,7 +7,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
-
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView resultTv,solutionTv;
     MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
@@ -71,6 +72,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         solutionTv.setText(dataToCalculate);
+
+        String finalResult=getResult(dataToCalculate);
+
+        if (!finalResult.equals("Error")){
+            resultTv.setText(finalResult);
+        }
+
+    }
+    String getResult(String data){
+        try {
+            Context context=Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable=context.initStandardObjects();
+            String finalResult=context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+            if (finalResult.endsWith(".0")){
+                finalResult=finalResult.replace(".0","");
+            }
+            return finalResult;
+        }catch (Exception e){
+            return "Error";
+        }
 
     }
 }
